4cfffdad8746740229e4e6eef7a2188334c382e5
elod <elod.pall@tu-berlin.de>
2018-04-26 20:42:56 +0200

    redesign data structures and handling to use arrays for sampling and max

--- ec_grasp_planner/src/multi_object_params.py
+++ ec_grasp_planner/src/multi_object_params.py
@@ -4,6 +4,7 @@ import yaml
 # import rospy
 import math
 import numpy as np
+import tf
 from tf import transformations as tra
 import operator
 import random
@@ -28,7 +29,12 @@ class multi_object_params:
     def __init__(self, file_name = "object_param.yaml"):
         self.file_name = file_name
 
+    # ================================================================================================
+    def transform_msg_to_homogenous_tf(self, msg):
+        return np.dot(tra.translation_matrix([msg.translation.x, msg.translation.y, msg.translation.z]),
+                      tra.quaternion_matrix([msg.rotation.x, msg.rotation.y, msg.rotation.z, msg.rotation.w]))
 
+    ## --------------------------------------------------------- ##
     #load parameters for hand-object-strategy
     def load_object_params(self):
         file = pkg_path + '/data/'+ self.file_name
@@ -39,17 +45,18 @@ class multi_object_params:
             except yaml.YAMLError as exc:
                 print(exc)
 
+## --------------------------------------------------------- ##
+    # return 0 or 1 if strategy is applicable on the object
+    # if there is a list of possible outcomes thant the strategy is applicable
     def pdf_object_strategy(self, object):
-        # return 0 or 1 if strategy is applicable on the object
-        # if there is a list of possible outcomes thant the strategy is applicable
-
         if isinstance(object['success'], list):
             return 1
         else:
             return object['success']
 
-    def pdf_object_ec(self, object, ec):
-        # return probability based on object and ec features
+## --------------------------------------------------------- ##
+    # return probability based on object and ec features
+    def pdf_object_ec(self, object, ec_frame):
         h = -1
         success = object['success']
         print object
@@ -59,7 +66,7 @@ class multi_object_params:
             obj_x_axis = object_frame[0:3, 0]
 
             for idx, val in enumerate(object['angle']):
-                ec_x_axis = ec[0:3, 0]
+                ec_x_axis = ec_frame[0:3, 0]
                 angle_epsilon = object['epsilon']
 
                 if (math.fabs(angle_between(obj_x_axis, ec_x_axis) - math.radians(val))
@@ -76,48 +83,96 @@ class multi_object_params:
 
         return h
 
+## --------------------------------------------------------- ##
     #object-envirtionment-hand based heuristic valeu for grasping
-    def heuristic(self, object, ec, strategy, hand):
+    def heuristic(self, object, ec_frame, strategy, hand):
 
         object_params = self.data[object['type']][hand][strategy]
         object_params['frame'] = object['frame']
         h = 1
 
-        h = h * self.pdf_object_strategy(object_params) * self.pdf_object_ec(object_params, ec)
+        h = h * self.pdf_object_strategy(object_params) * self.pdf_object_ec(object_params, ec_frame)
 
         return h
 
+## --------------------------------------------------------- ##
     # find the max probability and if there are more than one return one randomly
-    def argmax_h(self, grasp_dict):
+    def argmax_h(self, h_matrix):
         # find max probablity in list
-        max_probability = max(grasp_dict.iteritems(), key=operator.itemgetter(1))[1]
-        print(max_probability)
 
-        # select all items with max probability
-        max_probability_dict = {k:v for k,v in grasp_dict.items() if float(v) == max_probability}
-        print (max_probability_dict)
+
+
+        # max_probability_indexes = np.argwhere(h_matrix == h_matrix.argmax())
+        max_prob = h_matrix.max()
+        h_matrix[h_matrix!=max_prob] = 0;
+
+            #max(h_matrix.iteritems(), key=operator.itemgetter(1))[1]
+        # print(max_probability)
+        #
+        # # select all items with max probability
+        # max_probability_dict = {k:v for k,v in h_matrix.items() if float(v) == max_probability}
+        # print (max_probability_dict)
 
         # select randomly one of the itmes
-        max_random_item = self.sample_from_pdf(max_probability_dict)
-        print (max_random_item)
+        ideces_sampled_max = self.sample_from_pdf(h_matrix)
+        print (ideces_sampled_max)
 
-        return max_random_item
+        return ideces_sampled_max
 
-    def sample_from_pdf(self, pdf_dict):
+## --------------------------------------------------------- ##
+    # samples from a pdf dictionary where the values are normalized
+    # returns the key of the sample
+    def sample_from_pdf(self, pdf_matrix):
+
+        pdf_array = np.reshape(pdf_matrix, pdf_matrix.shape[0]*pdf_matrix.shape[1] )
 
         # normalize pdf
-        pdf_normalized = pdf_dict
-        sum_ = sum(pdf_dict.values())
-        pdf_normalized.update((x, y/sum_) for x, y in pdf_normalized.items())
+        pdf_normalized = pdf_array/sum(pdf_array)
 
         # sample probabilistically
-        sampled_item = (np.random.choice(pdf_normalized.keys(), p=pdf_normalized.values()))
-        return sampled_item
+        # sampled_item = (np.random.choice(pdf_normalized.keys(), p=pdf_normalized.values()))
 
-    def process_objects_ecs(self, objects, ecs, h_process_type = "argmax", hand_type = "RBOHandP24"):
+        sampled_item = (np.random.choice(len(pdf_normalized), p=pdf_normalized))
+
+        return sampled_item // pdf_matrix.shape[1], sampled_item % pdf_matrix.shape[1]
+
+## --------------------------------------------------------- ##
+    # function called to process all objects and ECs
+    # assumption1: all objects are the same type
+    # objects is a dictionary with obilagorty keys: type, frame (in robot base frame)
+    # ecs is a list of graph nodes (see geometry_graph)
+    def process_objects_ecs(self, objects, ecs, graph_in_base, h_process_type="argmax", hand_type="RBOHandP24WAM"):
+
+        self.load_object_params()
+        h_matrix = np.zeros((len(objects),len(ecs)))
+
+        # check if given hand exist
+        for i,o in enumerate(objects):
+
+            # check if the given hand type for this object is set in the yaml
+            if not self.data[o["type"]].get(hand_type, 0):
+                print("The givne hand {} and object {} has no parameters set in the yaml {}".format(hand_type, o["type"], self.file_name))
+                return -1
+
+            for j,ec in enumerate(ecs):
+                # the ec frame must be in the same reference frame as the object
+                ec_frame_in_base = graph_in_base.dot(self.transform_msg_to_homogenous_tf(ec.transform))
+                h_matrix[i,j] = self.heuristic(o, ec_frame_in_base, ec.label, hand_type)
+
+        if h_process_type == "argmax":
+            object_index,  ec_index = self.argmax_h(h_matrix)
+            return (objects[object_index], ecs[ec_index])
+
+        elif h_process_type == "sample":
+            object_index, ec_index = self.sample_from_pdf(h_matrix)
+            return (objects[object_index], ecs[ec_index])
+
 
         return (objects[0],ecs[0])
 
+## --------------------------------------------------------- ##
+## ---------------------- Main ----------------------------- ##
+## --------------------------------------------------------- ##
 if __name__ == "__main__":
     # testing the code inline
     obj_tf = tra.concatenate_matrices(
