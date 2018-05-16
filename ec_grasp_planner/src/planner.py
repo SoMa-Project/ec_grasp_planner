1b7ffcb0dde071f266ecc58c9bc028b5b680e21a
test-user@kuka1 <test-user@kuka1.com>
2018-05-16 15:27:53 +0100

    cucumber grasping params

--- ec_grasp_planner/src/planner.py
+++ ec_grasp_planner/src/planner.py
@@ -336,7 +336,7 @@ def create_surface_grasp(object_frame, bounding_box, support_surface_frame, hand
 
     # 3b. We switch after a short time as this allows us to do a small, precise lift motion
 
-    control_sequence.append(ha.TimeSwitch('LiftHand', 'softhand_close', duration=2.5))
+    control_sequence.append(ha.TimeSwitch('LiftHand', 'softhand_close', duration=1.8))
     # control_sequence.append(ha.FramePoseSwitch('LiftHand', 'softhand_close', goal_is_relative='1', goal=move_up_after_contact_goal, epsilon='0.004', reference_frame="world"))
 
 
@@ -363,8 +363,8 @@ def create_surface_grasp(object_frame, bounding_box, support_surface_frame, hand
     # Set up goal for the CLASH hand
     #params for mango
     speed = np.array([30]) 
-    thumb_pos = np.array([ 0, 30, 50])
-    diff_pos = np.array([30, 30, 60])
+    thumb_pos = np.array([ 0, 60, 30])
+    diff_pos = np.array([50, 50, 30])
     thumb_contact_force = np.array([0]) 
     thumb_grasp_force = np.array([0]) 
     diff_contact_force = np.array([0]) 
