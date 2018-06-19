8b2b6fff987dffe9974b8ffc18ff5e0da93e7524
Ilia Krets <krets@campus.tu-berlin.de>
2018-06-19 15:01:46 +0200

    added parameters for gt ifco framename and method, changed saving to pandas

--- ec_grasp_planner/src/evaluate_vision.py
+++ ec_grasp_planner/src/evaluate_vision.py
@@ -6,6 +6,8 @@ from pregrasp_msgs import srv as vision_srv
 import tf
 from numpy.random.mtrand import choice
 import numpy as np
+import pandas as pd
+import os
 from tf import transformations as tra
 
 def add_two_ints_client(x, y):
@@ -18,7 +20,7 @@ def add_two_ints_client(x, y):
         print "Service call failed: %s"%e
 
 def usage():
-    return "%s [number_of_objects type_of_object numeber_of_experiments baseframe_name]"%sys.argv[0]
+    return "%s [number_of_objects type_of_object number_of_experiments baseframe_name ifco_gt_frame_name vision_method]"%sys.argv[0]
 
 def transform_msg_to_homogenous_tf(msg):
     return np.dot(tra.translation_matrix([msg.translation.x, msg.translation.y, msg.translation.z]), tra.quaternion_matrix([msg.rotation.x, msg.rotation.y, msg.rotation.z, msg.rotation.w]))
@@ -28,11 +30,13 @@ if __name__ == "__main__":
 
     # arg 2 nr objects in ifco
     # arg 1 object typeifco_gt
-    if len(sys.argv) == 5:
+    if len(sys.argv) == 7:
         nr_objects = int(sys.argv[1])
         object_type = str(sys.argv[2])
         nr_experiments = int(sys.argv[3])
         baseframe = str(sys.argv[4])
+        ifco_gt_frame_name = sys.arvg[5]
+        vision_method = sys.argv[6]
     else:
         print usage()
         sys.exit(1)
@@ -45,7 +49,7 @@ if __name__ == "__main__":
 
     time_ = rospy.Time(0)
 
-    ifco_gt_frame_name = "camera_link"#""ifco_static"
+    # ifco_gt_frame_name = "camera_link"#""ifco_static"
 
     # get IFCO ground truth (TF listener)
     tf_listener = tf.TransformListener()
@@ -58,6 +62,13 @@ if __name__ == "__main__":
 
     file_name = "visionExperiment.csv"
     file_object = open(file_name, 'a')
+
+    df_filename = 'vision_experiment_df.pickle'
+    if not os.path.exists(df_filename):
+        df = pd.DataFrame(columns=['object_type', 'ifco_ground_truth', 'ifco_observed', 'observed_object_count'])
+    else
+        df = pd.DataFrame.from_pickle(df_filename)
+
     ifco_gt_array = np.squeeze(np.asarray(ifco_in_base_grounde_truth))
     ifco_gt_str = np.array2string(ifco_gt_array, precision=2, separator=',', suppress_small=True)
     ifco_gt_str = ifco_gt_array.tolist()
@@ -91,10 +102,17 @@ if __name__ == "__main__":
 
         ifco_o_str = ifco_o_array.tolist()
 
-        data = object_type+ ", " + str(nr_objects) + ", " + str(ifco_gt_str) + ", " + str(nr_objects_observed) + ", " + str(ifco_o_str)
-        file_object.write(data)
+        df.append({
+            'object_type': object_type,
+            'observed_object_count': nr_objects,
+            'ifco_ground_truth': ifco_gt_array,
+            'ifco_observed': ifco_observed
+        })
+        # data = object_type+ ", " + str(nr_objects) + ", " + str(ifco_gt_str) + ", " + str(nr_objects_observed) + ", " + str(ifco_o_str)
+        # file_object.write(data)
 
         rate.sleep()
 
+    df.to_pickle(df_filename)
     print ("result: {} ".format(data))
     file_object.close()
