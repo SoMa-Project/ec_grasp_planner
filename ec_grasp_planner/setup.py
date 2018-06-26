7d00eee6efc5fdde8d33871df680d87661e272d7
Johannes Wortmann <johannes.b.wortmann@campus.tu-berlin.de>
2018-06-26 18:08:49 +0200

    export as python module...

---
+++ ec_grasp_planner/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['ec_grasp_planner'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
