713a4d7f1636dd6f557fd8d28fa48b066cd93357
p.triantafyllou <p.triantafyllou@ocado.com>
2018-10-08 16:08:01 +0100

    get CLASH hand recipes from iros-demo

--- ec_grasp_planner/src/ClashHandRecipes.py
+++ ec_grasp_planner/src/ClashHandRecipes.py
@@ -28,6 +28,7 @@ def create_surface_grasp(object_frame, bounding_box, handarm_params, object_type
     downward_force = getParam(obj_type_params, obj_params, 'downward_force')
     ee_in_goal_frame = getParam(obj_type_params, obj_params, 'ee_in_goal_frame')
     lift_time = getParam(obj_type_params, obj_params, 'short_lift_duration')
+    init_joint_config = handarm_params['init_joint_config']
 
     down_IFCO_speed = handarm_params['down_IFCO_speed']
     up_IFCO_speed = handarm_params['up_IFCO_speed']
@@ -52,9 +53,9 @@ def create_surface_grasp(object_frame, bounding_box, handarm_params, object_type
     # Set the twists to use TRIK controller with
 
     # Down speed is positive because it is defined on the EE frame
-    down_IFCO_twist = tra.translation_matrix([0, 0, down_IFCO_speed]);
+    down_IFCO_twist = np.array([0, 0, down_IFCO_speed, 0, 0, 0]);
     # Slow Up speed is also positive because it is defined on the world frame
-    up_IFCO_twist = tra.translation_matrix([0, 0, up_IFCO_speed]);
+    up_IFCO_twist = np.array([0, 0, up_IFCO_speed, 0, 0, 0]);
     
     # Set the frames to visualize with RViz
     rviz_frames = []
@@ -65,6 +66,12 @@ def create_surface_grasp(object_frame, bounding_box, handarm_params, object_type
     # assemble controller sequence
     control_sequence = []
 
+    # # 0. Go to initial nice mid-joint configuration
+    # control_sequence.append(ha.JointControlMode(goal = init_joint_config, goal_is_relative = '0', name = 'init', controller_name = 'GoToInitController'))
+    
+    # # 0b. Switch when config is reached
+    # control_sequence.append(ha.JointConfigurationSwitch('init', 'Pregrasp', controller = 'GoToInitController', epsilon = str(math.radians(1.0))))
+
     # 1. Go above the object - Pregrasp
     control_sequence.append(ha.InterpolatedHTransformControlMode(goal_, controller_name = 'GoAboveObject', goal_is_relative='0', name = 'Pregrasp'))
  
@@ -72,12 +79,10 @@ def create_surface_grasp(object_frame, bounding_box, handarm_params, object_type
     control_sequence.append(ha.FramePoseSwitch('Pregrasp', 'StayStill', controller = 'GoAboveObject', epsilon = '0.01'))
  
     # 2. Go to gravity compensation 
-    control_sequence.append(ha.InterpolatedHTransformControlMode(tra.translation_matrix([0, 0, 0]),
+    control_sequence.append(ha.CartesianVelocityControlMode(np.array([0, 0, 0, 0, 0, 0]),
                                              controller_name='StayStillCtrl',
-                                             goal_is_relative='1',
                                              name="StayStill",
-                                             reference_frame="EE",
-                                             v_max=down_IFCO_speed))
+                                             reference_frame="EE"))
 
     # 2b. Wait for a bit to allow vibrations to attenuate
     control_sequence.append(ha.TimeSwitch('StayStill', 'softhand_preshape', duration = handarm_params['stay_still_duration']))
@@ -109,12 +114,10 @@ def create_surface_grasp(object_frame, bounding_box, handarm_params, object_type
 
     # 4. Go down onto the object (relative in EE frame) - Godown
     control_sequence.append(
-        ha.InterpolatedHTransformControlMode(down_IFCO_twist,
+        ha.CartesianVelocityControlMode(down_IFCO_twist,
                                              controller_name='GoDown',
-                                             goal_is_relative='1',
                                              name="GoDown",
-                                             reference_frame="EE",
-                                             v_max=down_IFCO_speed))
+                                             reference_frame="EE"))
 
     # 4b. Switch when force-torque sensor is triggered
     force  = np.array([0, 0, downward_force, 0, 0, 0])
@@ -129,7 +132,7 @@ def create_surface_grasp(object_frame, bounding_box, handarm_params, object_type
 
     # 5. Lift upwards so the hand can close
     control_sequence.append(
-        ha.InterpolatedHTransformControlMode(up_IFCO_twist, controller_name='Lift1', goal_is_relative='1', name="LiftHand",
+        ha.CartesianVelocityControlMode(up_IFCO_twist, controller_name='Lift1', name="LiftHand",
                                              reference_frame="world"))
 
     # 5b. We switch after a short time 
@@ -177,37 +180,27 @@ def create_wall_grasp(object_frame, bounding_box, wall_frame, handarm_params, ob
     downward_force = getParam(obj_type_params, obj_params, 'downward_force')
     wall_force = getParam(obj_type_params, obj_params, 'wall_force')
     slide_IFCO_speed = getParam(obj_type_params, obj_params, 'slide_speed')
+    pre_approach_transform = getParam(obj_type_params, obj_params, 'pre_approach_transform')
     scooping_angle_deg = getParam(obj_type_params, obj_params, 'scooping_angle_deg')
 
-    pre_approach_transform = tra.concatenate_matrices(tra.translation_matrix([-0.20, 0, 0]), tra.rotation_matrix(
-                                                                                        math.radians(scooping_angle_deg), [0, 1, 0]), tra.rotation_matrix(math.radians(90.), [0, 0, 1]))
+    init_joint_config = handarm_params['init_joint_config']
 
+    
 
     thumb_pos_preshape = getParam(obj_type_params, obj_params, 'thumb_pos_preshape')
     post_grasp_transform = getParam(obj_type_params, obj_params, 'post_grasp_transform')
     
-    vision_params = {}
-    # if object_type in handarm_params:
-    #     vision_params = handarm_params[object_type]
-    # offset = getParam(vision_params, handarm_params['object'], 'obj_bbox_uncertainty_offset')
-    # if abs(object_frame[:3,0].dot(wall_frame[:3,0])) > abs(object_frame[:3,1].dot(wall_frame[:3,0])):
-    #     pre_approach_transform[2,3] = pre_approach_transform[2,3] - bounding_box.y/2 - offset 
-    # else:
-    #     pre_approach_transform[2,3] = pre_approach_transform[2,3] - bounding_box.x/2 - offset
-
     rotate_time = handarm_params['rotate_duration']
     down_IFCO_speed = handarm_params['down_IFCO_speed']
 
     # Set the twists to use TRIK controller with
 
-    # Down speed is positive because it is defined on the EE frame
-    down_IFCO_twist = tra.translation_matrix([0, 0, down_IFCO_speed]);
+    # Down speed is negative because it is defined on the world frame
+    down_IFCO_twist = np.array([0, 0, -down_IFCO_speed, 0, 0, 0])
     
     # Slide speed is positive because it is defined on the EE frame + rotation of the scooping angle    
-    slide_IFCO_twist = tra.rotation_matrix(math.radians(-scooping_angle_deg), [1, 0, 0]).dot(tra.translation_matrix([0, 0, slide_IFCO_speed]))
-    slide_IFCO_twist = tra.translation_matrix(tra.translation_from_matrix(slide_IFCO_twist))
-
-
+    slide_IFCO_twist_matrix = tra.rotation_matrix(math.radians(scooping_angle_deg), [1, 0, 0]).dot(tra.translation_matrix([0, 0, slide_IFCO_speed]))
+    slide_IFCO_twist = np.array([slide_IFCO_twist_matrix[0,3], slide_IFCO_twist_matrix[1,3], slide_IFCO_twist_matrix[2,3], 0, 0, 0 ])
     
     rviz_frames = []
 
@@ -218,6 +211,7 @@ def create_wall_grasp(object_frame, bounding_box, wall_frame, handarm_params, ob
         ec_frame = ec_frame.dot(hand_transform)
 
         pre_approach_pose = ec_frame.dot(pre_approach_transform)
+
     else:
         pre_approach_pose = pre_grasp_pose
 
@@ -229,15 +223,30 @@ def create_wall_grasp(object_frame, bounding_box, wall_frame, handarm_params, ob
 
     control_sequence = []
 
+    # # 0. Go to initial nice mid-joint configuration
+    # control_sequence.append(ha.JointControlMode(goal = init_joint_config, goal_is_relative = '0', name = 'init', controller_name = 'GoToInitController'))
+    
+    # # 0b. Switch when config is reached
+    # control_sequence.append(ha.JointConfigurationSwitch('init', 'Pregrasp', controller = 'GoToInitController', epsilon = str(math.radians(1.0))))
+
     # 1. Go above the object
     control_sequence.append(
         ha.InterpolatedHTransformControlMode(pre_approach_pose, controller_name='GoAboveObject', goal_is_relative='0',
-                                             name="PreGrasp"))
+                                             name="Pregrasp"))
 
     # 1b. Switch when hand reaches the goal pose
-    control_sequence.append(ha.FramePoseSwitch('PreGrasp', 'softhand_pretension', controller='GoAboveObject', epsilon='0.01'))
+    control_sequence.append(ha.FramePoseSwitch('Pregrasp', 'StayStill', controller='GoAboveObject', epsilon='0.01'))
 
-    # 2. Pretension
+    # 2. Go to gravity compensation 
+    control_sequence.append(ha.CartesianVelocityControlMode(np.array([0, 0, 0, 0, 0, 0]),
+                                             controller_name='StayStillCtrl',
+                                             name="StayStill",
+                                             reference_frame="EE"))
+
+    # 2b. Wait for a bit to allow vibrations to attenuate
+    control_sequence.append(ha.TimeSwitch('StayStill', 'softhand_pretension', duration = handarm_params['stay_still_duration']))
+
+    # 3. Pretension
     speed = np.array([20]) 
     thumb_pos = np.array([ 0, 0, 0])
     diff_pos = np.array([0, 0, 15])
@@ -258,18 +267,17 @@ def create_wall_grasp(object_frame, bounding_box, wall_frame, handarm_params, ob
                                                                             thumb_pretension, diff_pretension, force_feedback_ratio, 
                                                                             prox_level, touch_level, mode, command_count)), name  = 'softhand_pretension'))
 
-    # 2b. Switch when hand closing time ends
+    # 3b. Switch when hand closing time ends
     control_sequence.append(ha.TimeSwitch('softhand_pretension', 'GoDown', duration = handarm_params['hand_closing_duration']))
 
 
-    # 3. Go down onto the object/table, in world frame
-    control_sequence.append( ha.InterpolatedHTransformControlMode(down_IFCO_twist,
+    # 4. Go down onto the object/table, in world frame
+    control_sequence.append( ha.CartesianVelocityControlMode(down_IFCO_twist,
                                              controller_name='GoDown',
-                                             goal_is_relative='1',
                                              name="GoDown",
-                                             reference_frame="EE"))
+                                             reference_frame="world"))
 
-    # 3b. Switch when force threshold is exceeded
+    # 4b. Switch when force threshold is exceeded
     force = np.array([0, 0, downward_force, 0, 0, 0])
     control_sequence.append(ha.ForceTorqueSwitch('GoDown',
                                                  'CloseBeforeSlide',
@@ -280,7 +288,7 @@ def create_wall_grasp(object_frame, bounding_box, wall_frame, handarm_params, ob
                                                  frame_id='world',
                                                  port='2'))
 
-    # 4. Close a bit before sliding
+    # 5. Close a bit before sliding
     speed = np.array([30]) 
     thumb_pos = thumb_pos_preshape
     diff_pos = np.array([10, 15, 0])
@@ -301,23 +309,23 @@ def create_wall_grasp(object_frame, bounding_box, wall_frame, handarm_params, ob
                                                                             thumb_pretension, diff_pretension, force_feedback_ratio, 
                                                                             prox_level, touch_level, mode, command_count)), name  = 'CloseBeforeSlide'))
 
-    # 4b. Time switch
+    # 5b. Time switch
     control_sequence.append(ha.TimeSwitch('CloseBeforeSlide', 'SlideToWall', duration = handarm_params['hand_closing_duration']))
 
 
-    # 5. Go towards the wall to slide object to wall
+    # 6. Go towards the wall to slide object to wall
     control_sequence.append(
-        ha.InterpolatedHTransformControlMode(slide_IFCO_twist, controller_name='SlideToWall', goal_is_relative='1',
+        ha.CartesianVelocityControlMode(slide_IFCO_twist, controller_name='SlideToWall',
                                              name="SlideToWall", reference_frame="EE"))
 
-    # 5b. Switch when the f/t sensor is triggered with normal force from wall
+    # 6b. Switch when the f/t sensor is triggered with normal force from wall
     force = np.array([0, 0, wall_force, 0, 0, 0])
     control_sequence.append(ha.ForceTorqueSwitch('SlideToWall', 'softhand_close', 'ForceSwitch', goal=force,
                                                  norm_weights=np.array([0, 0, 1, 0, 0, 0]),
                                                  jump_criterion="THRESH_UPPER_BOUND", goal_is_relative='1',
                                                  frame_id='world', frame=wall_frame, port='2'))
 
-    # 6. Close the hand
+    # 7. Close the hand
     speed = np.array([30]) 
     thumb_pos = np.array([ 0, 50, 30])
     diff_pos = np.array([55, 50, 20])
@@ -339,14 +347,14 @@ def create_wall_grasp(object_frame, bounding_box, wall_frame, handarm_params, ob
                                                                             prox_level, touch_level, mode, command_count)), name  = 'softhand_close'))
 
 
-    # 6b. Switch when hand closing time ends
+    # 7b. Switch when hand closing time ends
     control_sequence.append(ha.TimeSwitch('softhand_close', 'PostGraspRotate', duration = handarm_params['hand_closing_duration']))
     
-    # 7. Rotate hand after closing and before lifting it up relative to current hand pose
+    # 8. Rotate hand after closing and before lifting it up relative to current hand pose
     control_sequence.append(
-        ha.InterpolatedHTransformControlMode(post_grasp_transform, controller_name='PostGraspRotate', name='PostGraspRotate', goal_is_relative='1', reference_frame='EE'))
+        ha.CartesianVelocityControlMode(post_grasp_transform, controller_name='PostGraspRotate', name='PostGraspRotate', reference_frame='EE'))
 
-    # 7b. Switch when hand rotated
+    # 8b. Switch when hand rotated
     control_sequence.append(ha.TimeSwitch('PostGraspRotate', 'GoUp', duration = rotate_time))
 
     return control_sequence, rviz_frames
