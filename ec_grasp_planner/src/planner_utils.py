0b21748a9c7046f3f82da8c44860dd5d2b56edd9
ninafiona <ninafiona@gmx.de>
2020-11-12 11:19:36 +0100

    planner_utils adaptation

--- ec_grasp_planner/src/planner_utils.py
+++ ec_grasp_planner/src/planner_utils.py
@@ -14,16 +14,24 @@ def fromMsgToMatrix(p):
     """
     # from quaternion to rotation matrix
     # implementation as in KDL:
-    # x2 = p.orientation.x**2
-    # y2 = p.orientation.y**2
-    # z2 = p.orientation.z**2
-    # w2 = p.orientation.w**2
-    # return numpy.array([[w2+x2-y2-z2, 2*x*y-2*w*z, 2*x*z+2*w*y, p.position.x],
-    #                     [2*x*y+2*w*z, w2-x2+y2-z2, 2*y*z-2*w*x, p.position.y],
-    #                     [2*x*z-2*w*y, 2*y*z+2*w*x, w2-x2-y2+z2, p.position.z],
-    #                     [0,0,0,1]])
-    q = np.array([p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w])
-    return tra.quaternion_matrix(q)
+    x = p.orientation.x
+    y = p.orientation.y
+    z = p.orientation.z
+    w = p.orientation.w
+
+    x2 = x**2
+    y2 = y**2
+    z2 = z**2
+    w2 = w**2
+    return np.array([[w2+x2-y2-z2, 2*x*y-2*w*z, 2*x*z+2*w*y, p.position.x],
+                    [2*x*y+2*w*z, w2-x2+y2-z2, 2*y*z-2*w*x, p.position.y],
+                    [2*x*z-2*w*y, 2*y*z+2*w*x, w2-x2-y2+z2, p.position.z],
+                    [0,0,0,1]])
+
+    # # from msg to quaternion:
+    # q = np.array([p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w])
+    # # from quaternion to rotation matrix:
+    # return tra.quaternion_matrix(q)
 
 def fromMatrixToMsg(m):
     """
@@ -31,49 +39,50 @@ def fromMatrixToMsg(m):
     :type m: :func:`numpy.array`
     :return: ROS Pose message
 
-    Convert a numpy 4x4 array to a pose represented as a ROS Pose message.
+    Convert a numpy 4x4 array (rotation matrix) to a pose represented as a ROS Pose message.
     Replaces former operation pm.toMsg(pm.fromMatrix(htf)) (KDL)
     """
     p = Pose()
     # from rotation matrix to quaternion
-    q = tra.quaternion_from_matrix(m[0:3][0:3])
+    #q = tra.quaternion_from_matrix(m[0:3][0:3])
     # implementation as in KDL: 
-    # trace = m[0,0] + m[1,1] + m[2,2]
-    # epsilon=1E-12
-    # if trace > epsilon:
-    #     s = 0.5 / sqrt(trace + 1.0)
-    #     w = 0.25 / s
-    #     x = ( m[2,1] - m[1,2] ) * s
-    #     y = ( m[0,2] - m[2,0] ) * s
-    #     z = ( m[1,0] - m[0,1] ) * s
-    # else:
-    #     if m[0,0] > m[1,1] and m[0,0] > m[2,2]:
-    #         s = 2.0 * sqrt( 1.0 + m[0,0] - m[1,1] - m[2,2])
-    #         w = (m[2,1] - m[1,2] ) / s
-    #         x = 0.25 * s
-    #         y = (m[0,1] + m[1,0] ) / s
-    #         z = (m[0,2] + m[2,0] ) / s
-    #     elif m[1,1] > m[2,2]:
-    #         s = 2.0 * sqrt( 1.0 + m[1,1] - m[0,0] - m[2,2])
-    #         w = (m[0,2] - m[2,0] ) / s
-    #         x = (m[0,1] + m[1,0] ) / s
-    #         y = 0.25 * s
-    #         z = (m[1,2] + m[2,1] ) / s
-    #     else:
-    #         s = 2.0 * sqrt( 1.0 + m[2,2] - m[0,0] - m[1,1] )
-    #         w = (m[1,0] - m[0,1] ) / s
-    #         x = (m[0,2] + m[2,0] ) / s
-    #         y = (m[1,2] + m[2,1] ) / s
-    #         z = 0.25 * s
-    # p.orientation.x = x
-    # p.orientation.y = y 
-    # p.orientation.z = z
-    # p.orientation.w = w
-    
-    p.orientation.x = q[0]
-    p.orientation.y = q[1]
-    p.orientation.z = q[2]
-    p.orientation.w = q[3]
+    trace = m[0,0] + m[1,1] + m[2,2]
+    epsilon=1E-12
+    if trace > epsilon:
+        s = 0.5 / sqrt(trace + 1.0)
+        w = 0.25 / s
+        x = ( m[2,1] - m[1,2] ) * s
+        y = ( m[0,2] - m[2,0] ) * s
+        z = ( m[1,0] - m[0,1] ) * s
+    else:
+        if m[0,0] > m[1,1] and m[0,0] > m[2,2]:
+            s = 2.0 * sqrt( 1.0 + m[0,0] - m[1,1] - m[2,2])
+            w = (m[2,1] - m[1,2] ) / s
+            x = 0.25 * s
+            y = (m[0,1] + m[1,0] ) / s
+            z = (m[0,2] + m[2,0] ) / s
+        elif m[1,1] > m[2,2]:
+            s = 2.0 * sqrt( 1.0 + m[1,1] - m[0,0] - m[2,2])
+            w = (m[0,2] - m[2,0] ) / s
+            x = (m[0,1] + m[1,0] ) / s
+            y = 0.25 * s
+            z = (m[1,2] + m[2,1] ) / s
+        else:
+            s = 2.0 * sqrt( 1.0 + m[2,2] - m[0,0] - m[1,1] )
+            w = (m[1,0] - m[0,1] ) / s
+            x = (m[0,2] + m[2,0] ) / s
+            y = (m[1,2] + m[2,1] ) / s
+            z = 0.25 * s
+    p.orientation.x = x
+    p.orientation.y = y 
+    p.orientation.z = z
+    p.orientation.w = w
+
+    # from quaternion to pose msg 
+    # p.orientation.x = q[0]
+    # p.orientation.y = q[1]
+    # p.orientation.z = q[2]
+    # p.orientation.w = q[3]
     p.position.x = m[0,3]
     p.position.y = m[1,3]
     p.position.z = m[2,3]
